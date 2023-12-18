fc02bee888d4d2fdd399aaa70160e9fbcca4907f
Sharonsweety1713 <sharonsweety1713@gmail.com>
2023-12-18 20:32:18 +0530

    Day-7

---
+++ Day-7/my/src/components/claass.jsx
@@ -0,0 +1,14 @@
+import {Component} from 'react';
+class cc extends Component
+{
+    return(
+        <div>
+
+
+
+
+ </div>
+    )
+
+}
+export default cc;
---
+++ Day-7/my/src/components/func.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect } from 'react';
+
+const func=()=>{
+    return(
+        useEffect
+<div>
+    
+</div>
+    )
+}
+export default func;
